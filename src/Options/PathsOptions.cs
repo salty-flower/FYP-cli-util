fe861e13e5ef9dc0d206f9e50992e696bd2246bf
salty-flower <95509524+salty-flower@users.noreply.github.com>
2025-03-18 22:59:17 +0800

    fix: add python dir

--- src/Options/PathsOptions.cs
+++ src/Options/PathsOptions.cs
@@ -1,9 +1,12 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace DataCollection.Options;
 
+[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)]
 public class PathsOptions
 {
-    public string PaperMetadataDir { get; set; } = "../paper-metadata";
-    public string PdfDataDir { get; set; } = "../pdfdata";
-    public string PaperBinDir { get; set; } = "../paper-bin";
+    public string PaperMetadataDir { get; set; } = string.Empty;
+    public string PdfDataDir { get; set; } = string.Empty;
+    public string PaperBinDir { get; set; } = string.Empty;
     public string PythonDLL { get; set; } = string.Empty;
 }
