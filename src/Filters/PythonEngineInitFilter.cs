b4073ac1628d4bf7374abd6e3ed3104e7af334a3
salty-flower <95509524+salty-flower@users.noreply.github.com>
2025-04-30 15:40:05 +0800

    refactor: enhance python loading logic

--- src/Filters/PythonEngineInitFilter.cs
+++ src/Filters/PythonEngineInitFilter.cs
@@ -1,23 +1,43 @@
-﻿using System.Threading;
+﻿using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using ConsoleAppFramework;
 using DataCollection.Options;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Python.Runtime;
 
 namespace DataCollection.Filters;
 
-internal class PythonEngineInitFilter(ConsoleAppFilter next, IOptionsSnapshot<PathsOptions> pathOpt)
-    : ConsoleAppFilter(next)
+internal class PythonEngineInitFilter(
+    ConsoleAppFilter next,
+    IOptionsSnapshot<PathsOptions> pathOpt,
+    ILogger<PythonEngineInitFilter> logger
+) : ConsoleAppFilter(next)
 {
     public override async Task InvokeAsync(
         ConsoleAppContext context,
         CancellationToken cancellationToken
     )
     {
-        Runtime.PythonDLL = pathOpt.Value.PythonDLL;
+        // Pre-check
+        var declaredDllPath = pathOpt.Value.PythonDLL;
+        if (!Path.IsPathRooted(declaredDllPath))
+            declaredDllPath = Path.GetFullPath(
+                Path.Combine(BuildConstants.SolutionDirectory, declaredDllPath)
+            );
+
+        if (!File.Exists(declaredDllPath))
+        {
+            logger.LogError("Python DLL not found at {path}", declaredDllPath);
+            return;
+        }
+
+        logger.LogInformation("Python DLL found at {path}", declaredDllPath);
+        Runtime.PythonDLL = declaredDllPath;
         PythonEngine.Initialize();
         PythonEngine.BeginAllowThreads();
+        logger.LogInformation("Python engine initialized successfully");
 
         await Next.InvokeAsync(context, cancellationToken);
     }
